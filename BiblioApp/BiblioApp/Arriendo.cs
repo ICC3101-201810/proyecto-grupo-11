a5cfc3fc26e8cd0403b046187e199328b10f9393
joaquin tellez <jatellez@miuandes.cl>
2018-04-20 17:02:31 -0300

    Clase Arriendo

---
+++ BiblioApp/BiblioApp/Arriendo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioApp
+{
+    class Arriendo
+    {
+        DateTime FechaArriendo;
+
+        public Arriendo(DateTime MiFechaArriendo)
+        {
+            FechaArriendo = MiFechaArriendo;
+        }
+    }
+}
