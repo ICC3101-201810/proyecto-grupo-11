a2715e0bbcb28f03999d9643ef99a12644605cc8
joaquin tellez <jatellez@miuandes.cl>
2018-04-19 22:36:58 -0300

    MaterialEducativo

---
+++ BiblioApp/BiblioApp/MaterialEducativo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioApp
+{
+    class MaterialEducativo
+    {
+        string Autor;
+        string FechaCreacion;
+        int Calificacion;
+        List<string> Comentario;
+
+        public MaterialEducativo(string MiAutor, string MiFechaCreacion, int MiCalificacion, List<string> MiComentario)
+        {
+            Autor = MiAutor;
+            FechaCreacion = MiFechaCreacion;
+            Calificacion = MiCalificacion;
+            Comentario = new List<string>(MiComentario);
+        }
+
+
+    }
+}
