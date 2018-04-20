032169d31bb31eddafa264a3ef30a7b2483e9df7
Santiago Figueroa <37884952+SantiagoFigueroaMc@users.noreply.github.com>
2018-04-19 22:26:25 -0300

    colores_mensajes_prog_principal_prueba

--- BiblioApp/BiblioApp/Program.cs
+++ BiblioApp/BiblioApp/Program.cs
@@ -10,6 +10,38 @@ namespace BiblioApp
     {
         static void Main(string[] args)
         {
+
+        }
+
+        static void ROJO(string mensaje)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(mensaje);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void AMARILLO(string mensaje)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(mensaje);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void AZUL(string mensaje)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(mensaje);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void VERDE(string mensaje)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(mensaje);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void GRIS(string mensaje)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(mensaje);
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
