b587bec3617bb1edebdefdf205ee506ba310c04b
Santiago Figueroa <37884952+SantiagoFigueroaMc@users.noreply.github.com>
2018-05-11 17:43:00 -0300

    .

---
+++ Biblio_app_windows_form/Biblio_app_windows_form/AgregarUsuarioEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio_app_windows_form
+{
+    class AgregarUsuarioEventArgs : EventArgs
+    {
+        public string Rut { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Usuario { get; set; }
+        public string Password { get; set; }
+    }
+}
