845d6a9d942e99ced3808deae490557a681fd959
joaquin tellez <jatellez@miuandes.cl>
2018-05-11 14:41:09 -0300

    ya no se que esta pasando

---
+++ Biblio_app_windows_form/Biblio_app_windows_form/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio_app_windows_form
+{
+    class AdminController
+    {
+        List<Alumno> Usuario;
+        List<Administrador> Admin;
+        vista_administrador vista_Administrador;
+
+        public AdminController(vista_administrador vista_Administrador)
+        {
+            Usuario = new List<Alumno>();
+            Admin = new List<Administrador>();
+            this.vista_Administrador = vista_Administrador;
+            this.vista_Administrador.
+        }
+    }
+}
